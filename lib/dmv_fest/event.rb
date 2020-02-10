e6ff46acda50c3f0b03fc181ad2660211efc867f
Brianna Jordan <bryvjordan9@gmail.com>
2020-02-10 18:03:13 +0000

    commented out require dmv-fest

--- lib/dmv_fest/event.rb
+++ lib/dmv_fest/event.rb
@@ -1,6 +1,19 @@
+require 'pry'
+
 class DmvFest::Event
-  attr_accessor 
+  attr_accessor :name, :price, :line_up, :url
 
+  def self.year
+    # puts <<-DOC.gsub /^\s*/,''
+    #   1. event 1
+    #   2. event 2
+    # DOC
+    event_1 = self.new
+    event_1.name = "Capital Jazz Fest"
+    event_1.price = "TBA"
+    event_1.line_up = "The talent lineup for the 2020 festival will be announced in spring 2020. Stay tuned."
+    event_1.url = "https://www.capitaljazz.com/fest/2020/talent.php"
 
+  end
 
 end
