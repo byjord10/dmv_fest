1e7cddc6f36d1cc695bd0869ca3b2dcb0149ba6d
Brianna Jordan <bryvjordan9@gmail.com>
2020-02-11 00:56:09 +0000

    usuing binding.pry in event.rb

--- lib/dmv_fest/event.rb
+++ lib/dmv_fest/event.rb
@@ -1,31 +1,44 @@
+require 'pry'
 class DmvFest::Event
-  attr_accessor :name, :price, :line_up
+  attr_accessor :name, :price, :line_up, :url
 
+  @@all = []
+
+  def initialize(name, price, line_up, url)
+    @name = name
+    @phone = phone
+    @line_up = line_up
+    @url = url
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
   def self.year
     self.scrape_events
   end
 
   def self.scrape_events
-    events = []
-
     events << self.scrape_jazz
     events << self.scrape_broccoli
 
     events
   end
 
-
-  def self.scrape_jazz
+binding.pry
+  def self.scrape_jazz(event_1)
+    binding.pry
     doc = Nokogiri::HTML(open("https://www.capitaljazz.com/fest/2020/talent.php"))
 
-    event = self.new
-    event.name = doc.search("text-center no-space-bottom").text.strip
-    event.price = doc.search("Ticket Info &amp; Prices").text
-    event.line_up = doc.search("text-center").text
-    event.url = doc.search("capitaljazz.com")
+    event_1 = self.new
+    event_1.name = doc.search("text-center no-space-bottom").text.strip
+    event_1.price = doc.search("Ticket Info &amp; Prices").text
+    event_1.line_up = doc.search("text-center").text
+    event_1.url = doc.search("capitaljazz.com")
 
-    event
+    return events
   end
 
   def self.scrape_broccoli
@@ -33,7 +46,7 @@ class DmvFest::Event
 
     event = self.new
     event.name = doc.search(" ").text.strip
-    event.price = doc.search(" ").text
+    event.price = doc.search("col-sm-2 nopadding text-center xtix-price ").text
     event.line_up = doc.search("").text
     event.url = " "
 
