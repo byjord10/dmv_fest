61eb620543c4f3293a74e6b2ce3dcdc21605cf94
Brianna Jordan <bryvjordan9@gmail.com>
2020-02-15 16:28:21 +0000

    quick save, changing scraper

--- lib/dmv_fest/event.rb
+++ lib/dmv_fest/event.rb
@@ -1,15 +1,16 @@
 class DmvFest::Event
-  attr_accessor :name, :location, :date, :ticket_type, :ticket_price, :line_up, :url
-
-  ATTRIBUTES = [
-    "name:",
-    "location:",
-    "date:",
-    "ticket_type:",
-    "ticket_price:",
-    "line_up:",
-    "url:"
-  ]
+  attr_accessor :name
+  attr_reader :location, :date, :line_up, :url
+  #
+  # ATTRIBUTES = [
+  #   "name:",
+  #   "location:",
+  #   "date:",
+  #   "ticket_type:",
+  #   "ticket_price:",
+  #   "line_up:",
+  #   "url:"
+  # ]
 
   @@name = []
 
@@ -26,23 +27,21 @@ class DmvFest::Event
     @@name << self
   end
 
-  def event=(event)
-    @event = event
-  end
-
-  # def self.scrape_event_by_name
-  #
-  #   # self.all.detect do |event|
-  #   #   @@name == name
-  #   end
+  # def event=(event)
+  #   @event = event
   # end
 
-  def self.find_by_name(name)
-    @@name.find {|event| event.name == name }
-  end #See if app would function without this method
-
-  def to_param
-    name.downcase
+  def self.scrape_event_by_name(name)
+    @@name.detect {|event| event.name == name}
   end
-end 
-   #See if app would function witout this method
+
+  # def self.find_by_name(name)
+  #   @@name.find {|event| event.name == name }
+  # end #See if app would function without this method
+
+  # def to_param
+  #   name.downcase
+  # end
+  #See if app would function witout this method
+
+end
--- lib/dmv_fest/scrape_event.rb
+++ lib/dmv_fest/scrape_event.rb
@@ -5,53 +5,19 @@ class DmvFest::ScrapeEvent
 
   attr_accessor :parse_page
 
-  def initialize
-    doc = HTTParty.get("")
+  deff initialize
+    doc = HTTParty.get("http://metromontage.com/list-of-music-festivals-in-d-c-maryland-and-northern-virginia/")
+    @parse_page ||= Nokogiri::HTML(doc)
+
+  end
+end 
 
 
-    def scrape_event_by_name = {
+      doc = Nokogiri::HTML(open(" "))
 
-    "Charm City Bluegrass Festival" => {
-
-      doc = Nokogiri::HTML(open("https://www.musicfestivalwizard.com/festivals/charm-city-bluegrass-festival-2020/"))
-
-      event.name = doc.search("entry-title search-title").text
-      event.location = doc.search("search-meta").text
-      event.ticket_type = doc.search("ticket-type").text
-      event.ticket_price = doc.search("ticket-price").text
-      event.line_up = doc.search("strong").text
-      event.url = doc.search("https://www.mt.cm/2020-charm-city-bluegrass-festival")
-    },
-    "Something In The Water" => {
-
-      doc = Nokogiri::HTML(open("https://somethinginthewater.frontgatetickets.com/"))
-
-      event.name = doc.search("header-image").text.strip
-      event.location = doc.search("location").text
-      event.ticket_type = doc.search("omnibox__listing__section").text
-      event.ticket_price = doc.search("event-price col-xs-12 nopadding").text
+      event.name = doc.search("").text
+      event.location = doc.search("").text
+      event.ticket_type = doc.search("").text
+      event.ticket_price = doc.search("").text
       event.line_up = doc.search("").text
-      event.url = "https://seatgeek.com/something-in-the-water-festival-tickets "
-    },
-
-    "" => {
-
-      doc = Nokogiri::HTML(open())
-
-      event = self.new
-      event.name = doc.search(" ").text.strip
-      event.price = doc.search(" ").text
-      event.line_up = doc.search("").text
-      event.url = " "
-    },
-
-    "All Things Go Fall Classic" => {
-
-      doc = Nokogiri::HTML(open())
-
-      event = self.new
-      event.name = doc.search(" ").text.strip
-      event.price = doc.search(" ").text
-      event.line_up = doc.search("").text
-      event.url = " "
-    }
+      event.url = doc.search("")
